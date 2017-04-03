58aae61e9616ba18b716988dbc935d307572c16d
locona <locona.0810@gmail.com>
2017-04-04 01:23:41 +0900

    App installer

---
+++ bootstrap/app.zsh
@@ -0,0 +1,8 @@
+# Apptivate
+curl http://www.apptivateapp.com/resources/Apptivate.app.zip $HOME
+
+# Alfred
+curl https://cachefly.alfredapp.com/Alfred_3.3.1_806.dmg $HOME
+
+# AppCleaner
+curl https://freemacsoft.net/downloads/AppCleaner_3.4.zip $HOME
