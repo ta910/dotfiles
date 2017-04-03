0abcc8e12899a91c93ec08e3a56a8ac1e979334e
locona <locona.0810@gmail.com>
2017-04-04 01:16:52 +0900

    Add xcode command line

--- bootstrap/brew.sh
+++ bootstrap/brew.sh
@@ -3,6 +3,6 @@ if [ ! `which brew` ]; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   brew update
 fi
-
+xcode-select --install
 brew tap Homebrew/bundle
 brew bundle
