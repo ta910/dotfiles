5111c01720a27e1db4969361d4edebafe4c81ef6
locona <locona.0810@gmail.com>
2017-04-04 01:11:15 +0900

    Add postgresql

---
+++ bootstrap/postgresql.zsh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Postgres.app
+curl https://github.com/PostgresApp/PostgresApp/releases/download/v2.0.2/Postgres-2.0.2.dmg $HOME
+
+# sudo mkdir -p /etc/paths.d &&
+# echo /Applications/Postgres.app/Contents/Versions/latest/bin | sudo tee /etc/paths.d/postgresapp
+# Postico
+curl https://eggerapps.at/postico/download/ $HOME
