d89b5ed38f904e15cad77bc5a09f73eb23e765da
locona <locona.0810@gmail.com>
2017-04-04 21:04:28 +0900

    Add elasticsearch

--- zsh/src/exports.zsh
+++ zsh/src/exports.zsh
@@ -15,3 +15,5 @@ export GO15VENDOREXPERIMENT=1
 
 export LSCOLORS=exfxcxdxbxegedabagacad
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+
+export ELASTICPATH=/usr/local/bin/elasticsearch
