2f1b6bac2f278780fdcc5cd3f05930ac91215a67
locona <locona.0810@gmail.com>
2017-04-14 17:13:13 +0900

    change bootstrap script

--- bootstrap/vim.sh
+++ bootstrap/vim.sh
@@ -1,61 +1,42 @@
 #!/bin/sh
 
-if [ ! -d ~/.vim/bundle/ ]; then
-  mkdir -p ~/.vim/bundle
+if [ ! -d ~/.vim/dein/ ]; then
+  mkdir -p ~/.vim/dein
   curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-  sh installer.sh ~/.vim/bundle
+  sh installer.sh ~/.vim/dein
 fi
 
 function create_symlink() {
-  #===Docs
-  if [ ! -d ~/.vim/docs/ ]; then
-    mkdir -p ~/.vim/docs/
-  fi
-
-  docs=(vim-submode)
-  for doc in ${docs[@]}; do
-    echo "${doc}"
-    ln -sf ~/w/github.com/locona/dotfiles/vim/docs/${doc}.md ~/.vim/docs/${doc}.md
-  done
-
   #=== after
-  if [ -d ~/.vim/after/ ]; then
-    ln -sf ~/w/github.com/locona/dotfiles/vim/after/plugin/release-comment.vim ~/.vim/after/plugin/release-comment.vim
-  else
+  if [ ! -d ~/.vim/after/ ]; then
     mkdir -p ~/.vim/after/plugin
-    ln -sf ~/w/github.com/locona/dotfiles/vim/after/plugin/release-comment.vim /Users/locona/.vim/after/plugin/release-comment.vim
   fi
+	ln -sf ~/w/github.com/locona/dotfiles/vim/after/plugin/release-comment.vim ~/.vim/after/plugin/release-comment.vim
 
-  if [ -d ~/.vim/ftplugin ]; then
-    ln -sf $HOME/w/github.com/locona/dotfiles/vim/ftplugin/*.vim $HOME/.vim/ftplugin/
-  else
+  if [ ! -d ~/.vim/ftplugin ]; then
     mkdir -p ~/.vim/ftplugin
-    ln -sf $HOME/w/github.com/locona/dotfiles/vim/ftplugin/*.vim $HOME/.vim/ftplugin/
   fi
+	ln -sf $HOME/w/github.com/locona/dotfiles/vim/ftplugin/*.vim $HOME/.vim/ftplugin/
 
-  #=== colors
-  if [ -d ~/.vim/colors/ ]; then
-    ln -sf ~/w/github.com/locona/dotfiles/vim/colors/solarized.vim ~/.vim/colors
-  else
-    mkdir ~/.vim/colors/
-    ln -sf ~/w/github.com/locona/dotfiles/vim/colors/solarized.vim ~/.vim/colors
-  fi
-
-  files=(basic bundle bundle.config color indent keybind syntax)
-
+	if [ ! -d $HOME/.vim/vimrc.d/plugin ]; then
+		mkdir -p $HOME/.vim/vimrc.d/plugin
+	fi
+	files=(basic color indent keybind syntax)
+  #files=(basic bundle bundle.config color indent keybind syntax)
   for file in ${files[@]}; do
-    echo "${file}.vim"
-    ln -sf ~/w/github.com/locona/dotfiles/vim/${file}.vim ~/.vim/.vimrc.${file}
+    ln -sf ~/w/github.com/locona/dotfiles/vim/vimrc.d/${file}.vim ~/.vim/vimrc.d/
   done
+
+  tomls=(dein dein_lazy)
+  for toml in ${tomls[@]}; do
+    ln -sf ~/w/github.com/locona/dotfiles/vim/${toml}.toml ~/.vim
+  done
+
+  ln -sf $HOME/w/github.com/locona/dotfiles/vim/vimrc.d/plugin/*.vim $HOME/.vim/vimrc.d/plugin/
+
 }
 
-[ -e ~/.vimrc ]  || ln -s ~/w/github.com/locona/dotfiles/vimrc ~/.vimrc
-[ -e ~/.gvimrc ] || ln -s ~/w/github.com/locona/dotfiles/gvimrc ~/.gvimrc
-#=== Execute
-if [ -d ~/.vim/ ]; then
-  create_symlink
-else
-  echo "Not ~/.vim/ So make directory ~/.vim/"
-  mkdir ~/.vim/
-  create_eymlink
-fi
+[ -e ~/.vimrc ]  || ln -s ~/w/github.com/locona/dotfiles/vim/vimrc ~/.vimrc
+[ -e ~/.gvimrc ] || ln -s ~/w/github.com/locona/dotfiles/vim/gvimrc ~/.gvimrc
+
+create_symlink
