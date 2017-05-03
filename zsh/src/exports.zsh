b09a0b15fe31eb479c3730bced5cee096f3adf13
locona <locona.0810@gmail.com>
2017-05-03 20:21:13 +0900

    Update ELASTICPATH from brew install

--- zsh/src/exports.zsh
+++ zsh/src/exports.zsh
@@ -13,5 +13,5 @@ export PATH="$HOME/.anyenv/bin:$PATH"
 export LSCOLORS=exfxcxdxbxegedabagacad
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 export GOPATH=/Users/locona/gopus
-export ELASTICPATH=/usr/local/bin/elasticsearch
+export ELASTICPATH=/usr/local/Cellar/elasticsearch/5.3.2/bin
 # export XDG_CONFIG_HOME="~/.config"
